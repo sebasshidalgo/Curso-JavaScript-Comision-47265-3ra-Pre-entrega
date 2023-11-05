01f400540f3eb74a158318d5e96960dfd5db3452
Sebastián Hidalgo <sebastianm.hidalgo@gmail.com>
2023-11-05 00:05:41 -0300

    modificando ruta de img

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 // Creamos el array con los productos
 const productos = [
-    {modelo: "S21+", imagen: "../assets/img/s21+.png", pantalla: `Dynamic AMOLED 2X 6,7" Full HD+`, almacenamiento: "128 GB", memoria: "8 GB", bateria: "4800 mAh" , procesador: "Snapdragon 888" , camara: "12 mpx" , precio: 249, stock: 1},
-    {modelo: "S21 Ultra", imagen: "../assets/img/s21ultra.png", pantalla: `Dynamic AMOLED 2X 6,8" 120Hz WQHD+`, almacenamiento: "256 GB" , memoria: "12 GB", bateria: "5000 mAh" , procesador: "Exynos 2100", camara: "108 mpx" , precio: 499, stock: 1},
-    {modelo: "Z Fold 4", imagen: "../assets/img/zfold.png", pantalla: `Dynamic AMOLED 2X 7,6" Full HD+ y 120Hz` , almacenamiento: "512 GB", memoria: "12GB" , bateria: "4400 mAh", procesador: "Snapdragon 8+ Gen 1" , camara: "50 mpx" , precio: 799, stock: 1},
-    {modelo: "Z Flip 4", imagen: "../assets/img/zflip.png", pantalla: `Dynamic AMOLED 2X 6,7" Full HD+ y 120Hz`, almacenamiento: "256 GB", memoria: "8 GB", bateria: "3700 mAh", procesador: "Snapdragon 8+ Gen 1", camara: "12 mpx" , precio: 599, stock: 1},
-    {modelo: "iPhone 13", imagen: "../assets/img/13.png", pantalla: `Super Retina XDR OLED 6,1" Full HD+`, almacenamiento: "512 GB", memoria: "4 GB", bateria: "3227 mAh", procesador: "Apple A15 Bionic", camara: "12 mpx", precio: 1499, stock: 1},
-    {modelo: "iPhone 12 Pro", imagen: "../assets/img/12pro.png", pantalla: `Super Retina XDR OLED 6,1" Full HD`, almacenamiento: "256 GB", memoria: "6 GB", bateria: "2815 mAh", procesador: "Apple A14", camara: "12 mpx", precio: 1299, stock: 1},
-    {modelo: "iPhone 11", imagen: "../assets/img/11.png", pantalla: `IPS LCD 6,1" Full HD`, almacenamiento: "128 GB", memoria: "4 GB", bateria: "3110 mAh", procesador: "Apple A13 Bionic", camara: "12 mpx", precio: 1099, stock: 1},
-    {modelo: "iPhone 8 Plus", imagen: "../assets/img/8plus.png", pantalla: `IPS LCD 5,5" HD`, almacenamiento: "64 GB", memoria: "3 GB", bateria: "2700 mAh", procesador: "Apple A11 Bionic", camara: "12 mpx", precio: 799, stock: 1},
-    {modelo: "iPad 12", imagen: "../assets/img/ipad12.png", pantalla: `Liquid Retina XDR 12,9"`, almacenamiento: "512 GB", memoria: "16 GB", bateria: "5500 mAh", procesador: "Chip M1", camara: "12 mpx", precio: 899, stock: 1},
-    {modelo: "iPad 10 Pro", imagen: "../assets/img/ipad10pro.png", pantalla: `LCD IPS 10,5"`, almacenamiento: "256 GB", memoria: "8 GB", bateria: "5200 mAh", procesador: "Chip A10X Fusion", camara: "12 mpx", precio: 599, stock: 1},
-    {modelo: "iPad 10 Mini", imagen: "../assets/img/ipad10mini.png", pantalla: `LCD IPS 8,3"`, almacenamiento: "256 GB", memoria: "8 GB", bateria: "4700 mAh", procesador: "Chip A15 Bionic", camara: "12 mpx", precio: 499, stock: 1},
-    {modelo: "iPad 11 Pro", imagen: "../assets/img/ipad11pro.png", pantalla: `Liquid Retina IPS 11"`, almacenamiento: "512 GB", memoria: "16 GB", bateria: "5200 mAh", procesador: "Chip M1", camara: "12 mpx", precio: 799, stock: 1},
+    {modelo: "S21+", imagen: "./assets/img/s21+.png", pantalla: `Dynamic AMOLED 2X 6,7" Full HD+`, almacenamiento: "128 GB", memoria: "8 GB", bateria: "4800 mAh" , procesador: "Snapdragon 888" , camara: "12 mpx" , precio: 249, stock: 1},
+    {modelo: "S21 Ultra", imagen: "./assets/img/s21ultra.png", pantalla: `Dynamic AMOLED 2X 6,8" 120Hz WQHD+`, almacenamiento: "256 GB" , memoria: "12 GB", bateria: "5000 mAh" , procesador: "Exynos 2100", camara: "108 mpx" , precio: 499, stock: 1},
+    {modelo: "Z Fold 4", imagen: "./assets/img/zfold.png", pantalla: `Dynamic AMOLED 2X 7,6" Full HD+ y 120Hz` , almacenamiento: "512 GB", memoria: "12GB" , bateria: "4400 mAh", procesador: "Snapdragon 8+ Gen 1" , camara: "50 mpx" , precio: 799, stock: 1},
+    {modelo: "Z Flip 4", imagen: "./assets/img/zflip.png", pantalla: `Dynamic AMOLED 2X 6,7" Full HD+ y 120Hz`, almacenamiento: "256 GB", memoria: "8 GB", bateria: "3700 mAh", procesador: "Snapdragon 8+ Gen 1", camara: "12 mpx" , precio: 599, stock: 1},
+    {modelo: "iPhone 13", imagen: "./assets/img/13.png", pantalla: `Super Retina XDR OLED 6,1" Full HD+`, almacenamiento: "512 GB", memoria: "4 GB", bateria: "3227 mAh", procesador: "Apple A15 Bionic", camara: "12 mpx", precio: 1499, stock: 1},
+    {modelo: "iPhone 12 Pro", imagen: "./assets/img/12pro.png", pantalla: `Super Retina XDR OLED 6,1" Full HD`, almacenamiento: "256 GB", memoria: "6 GB", bateria: "2815 mAh", procesador: "Apple A14", camara: "12 mpx", precio: 1299, stock: 1},
+    {modelo: "iPhone 11", imagen: "./assets/img/11.png", pantalla: `IPS LCD 6,1" Full HD`, almacenamiento: "128 GB", memoria: "4 GB", bateria: "3110 mAh", procesador: "Apple A13 Bionic", camara: "12 mpx", precio: 1099, stock: 1},
+    {modelo: "iPhone 8 Plus", imagen: "./assets/img/8plus.png", pantalla: `IPS LCD 5,5" HD`, almacenamiento: "64 GB", memoria: "3 GB", bateria: "2700 mAh", procesador: "Apple A11 Bionic", camara: "12 mpx", precio: 799, stock: 1},
+    {modelo: "iPad 12", imagen: "./assets/img/ipad12.png", pantalla: `Liquid Retina XDR 12,9"`, almacenamiento: "512 GB", memoria: "16 GB", bateria: "5500 mAh", procesador: "Chip M1", camara: "12 mpx", precio: 899, stock: 1},
+    {modelo: "iPad 10 Pro", imagen: "./assets/img/ipad10pro.png", pantalla: `LCD IPS 10,5"`, almacenamiento: "256 GB", memoria: "8 GB", bateria: "5200 mAh", procesador: "Chip A10X Fusion", camara: "12 mpx", precio: 599, stock: 1},
+    {modelo: "iPad 10 Mini", imagen: "./assets/img/ipad10mini.png", pantalla: `LCD IPS 8,3"`, almacenamiento: "256 GB", memoria: "8 GB", bateria: "4700 mAh", procesador: "Chip A15 Bionic", camara: "12 mpx", precio: 499, stock: 1},
+    {modelo: "iPad 11 Pro", imagen: "./assets/img/ipad11pro.png", pantalla: `Liquid Retina IPS 11"`, almacenamiento: "512 GB", memoria: "16 GB", bateria: "5200 mAh", procesador: "Chip M1", camara: "12 mpx", precio: 799, stock: 1},
 ];
 
 // Traemos el div contenedor padre donde agregaremos los productos del array
